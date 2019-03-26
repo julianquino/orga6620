65963a18e24a20eee02079ff5beb1a616eddf310
Unknown <german7@protonmail.com>
2019-03-26 01:34:52 -0300

    Falta probar bien las salidas. Nada se probó en el emulador. Entre otras cosas.

---
+++ TP0/unix2dos.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define ERROR -1
+#define SALIDA_EXITOSA 0
+
+
+/**
+ * Procesa el archivo de entrada o el stream ingresado por stdin
+ *
+ * @param inputFile
+ * @param outputFile
+ * @return un código
+ */
+int processInput(FILE *inputFile, FILE *outputFile) {
+    int c;
+    while((c=fgetc(inputFile))!=EOF){
+        if(c=='\n'){
+            fprintf(outputFile,"\r\n");
+        }
+        else{
+            fprintf(outputFile,"%c",c);
+        }
+    }
+
+    if(fclose(inputFile)==EOF){
+        fprintf(stderr, "Error fclose: %s\n", strerror( errno ));
+        return ERROR;
+    }
+
+    if(outputFile != stdout){
+        if(fclose(outputFile)==EOF){
+            fprintf(stderr, "Error fclose: %s\n", strerror( errno));
+            return ERROR;
+        }
+    }
+
+    return SALIDA_EXITOSA;
+}
+
+int main(int argc, char *argv[]) {
+    int option = 0;
+    const char *short_opt = "i:o:hV";
+    struct option long_opt[] = {
+            {"version", no_argument,       NULL, 'V'},
+            {"help",    no_argument,       NULL, 'h'},
+            {"input",   required_argument, NULL, 'i'},
+            {"output",  required_argument, NULL, 'o'},
+            {NULL, 0,                      NULL, 0}
+    };
+    FILE *inputFile = NULL;
+    FILE *outputFile = NULL;
+
+    while ((option = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
+        switch (option) {
+            case 'V':
+                printf("TP #0 de la materia Organización de Computadoras \n");
+                printf("Alumnos: \n");
+                printf("	Bobadilla Catalan German\n	Del Carril Manuel \n	Quino Lopez Julian \n");
+                return 0;
+            case 'h':
+                printf("Usage: \n");
+                printf("	%s -h \n", argv[0]);
+                printf("	%s -V \n", argv[0]);
+                printf("	%s [options] \n", argv[0]);
+                printf("Options: \n");
+                printf("	-V, --version  Print version and quit. \n");
+                printf("	-h, --help     Print this information. \n");
+                printf("	-o, --output   Location of the output file. \n");
+                printf("	-i, --input    Location of the input file. \n");
+                return 0;
+            case 'i':
+                inputFile = fopen(optarg, "r");
+                if (inputFile == NULL) {
+                	fprintf(stderr, "Error archivo entrada: %s\n", strerror(errno));
+                }
+                break;
+            case 'o':
+                // verifico si existe el archivo
+                if (access(optarg, W_OK) != -1) {
+                    outputFile = fopen(optarg, "w+");
+                    if (outputFile == NULL) {
+                        fprintf(stderr, "Error archivo salida: %s\n", strerror(errno));
+                        return ERROR;
+                    }
+                }
+                break;
+            default:
+                // así está en el manual de getopt
+                abort();
+        }
+    }
+
+    if (inputFile == NULL) {
+        inputFile = stdin;
+    }
+
+    if (outputFile == NULL) {
+        outputFile = stdout;
+    }
+
+    if (processInput(inputFile, outputFile) == ERROR) {
+    	return ERROR;
+    }
+
+    return SALIDA_EXITOSA;
+}
